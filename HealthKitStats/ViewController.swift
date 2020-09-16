730cee97be30c7d0802dc1642b6f1c77260e7b22
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2020-09-16 17:48:29 +0100

    First

---
+++ HealthKitStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitStats
+//
+//  Created by Bailey Search on 16/09/2020.
+//  Copyright © 2020 Bailey Search. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
